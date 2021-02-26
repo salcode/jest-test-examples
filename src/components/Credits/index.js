16530d30861bb74a534be463af5a5cfc2c0470f7
Sal Ferrarello <sal@ironcodestudio.com>
2021-02-26 09:55:26 -0500

    Add Credits component
    See #1

---
+++ src/components/Credits/index.js
@@ -0,0 +1,12 @@
+export default function Credits() {
+  return (
+    <h3>
+      Built by <a href="https://salferrarello.com">
+        Sal Ferrarello
+      </a> (
+        <a href="https://twitter.com/salcode">@salcode</a>
+      )
+      using <a href="https://create-react-app.dev/">Create React App</a>
+    </h3>
+  );
+};
