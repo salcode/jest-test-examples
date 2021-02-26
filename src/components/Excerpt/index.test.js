44aab92c9fce66536f062f25f33f075395242fa0
Sal Ferrarello <sal@ironcodestudio.com>
2021-02-26 11:31:39 -0500

    Variablize values in Excerpt test
    See #3

--- src/components/Excerpt/index.test.js
+++ src/components/Excerpt/index.test.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom';
 import Excerpt from './';
 
 it('renders the correct length substring', () => {
+  const content = "Hi everybody";
+  const maxChars = 4;
+  const expected = "Hi";
+
   const container = document.createElement('div');
   const rendered = ReactDOM.render(
     <Excerpt
-      content="Hi everybody"
-      maxChars={4}
+      content={content}
+      maxChars={maxChars}
     />,
     container
   );
-  expect(container.textContent).toBe('Hi');
+  expect(container.textContent).toBe(expected);
 });
