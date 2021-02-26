ba3360e3d870621a82d40b50e4552145825079bd
Sal Ferrarello <sal@ironcodestudio.com>
2021-02-26 12:08:37 -0500

    Refactor Excerpt test suite for multiple tests
    Refactor Excerpt test suite to loop through many values and test each
    (similar to a data provider in PHPUnit)
    
    See #3

--- src/components/Excerpt/index.test.js
+++ src/components/Excerpt/index.test.js
@@ -1,19 +1,35 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render, unmountComponentAtNode } from 'react-dom';
 import Excerpt from './';
 
-it('renders the correct length substring', () => {
-  const content = "Hi everybody";
-  const maxChars = 4;
-  const expected = "Hi";
-
-  const container = document.createElement('div');
-  const rendered = ReactDOM.render(
-    <Excerpt
-      content={content}
-      maxChars={maxChars}
-    />,
-    container
-  );
-  expect(container.textContent).toBe(expected);
-});
+[
+  [ 'Hi everybody', 4, 'Hi' ],
+  [ '12 3456', 2, '12' ],
+  [ 'One two three', 4, 'One' ],
+  [ '      One two three', 3, 'One' ],
+  [ 'Hi everybody', 4, 'Hi' ],
+  [ '12 345678', 4, '12' ],
+  [ '      helllo hello', 10, 'helllo' ],
+  [ 'abc def  ', 10, 'abc def' ],
+  [ '<strong>One two three</strong>', 5, 'One' ],
+  [ '                ', 5, '' ],
+  [ 'One ', 4, 'One' ],
+  [ ' One ', 5, 'One' ],
+].map(
+  ([content, maxChars, expected], index) => {
+    const container = document.createElement('div');
+    it(`renders the correct length substring ${index}`, () => {
+      render(
+        <Excerpt
+          content={content}
+          maxChars={maxChars}
+        />,
+        container
+      );
+      expect(container.textContent).toBe(expected);
+    });
+    // Cleanup.
+    unmountComponentAtNode(container);
+    container.remove();
+  }
+);
