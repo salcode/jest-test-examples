60002bf039fa831d38d35b0a3fc754a4426a88d9
Sal Ferrarello <sal@ironcodestudio.com>
2021-02-26 14:55:32 -0500

    Add tests for Excerpt displaying ...

--- src/components/Excerpt/index.test.js
+++ src/components/Excerpt/index.test.js
@@ -1,8 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render, unmountComponentAtNode } from 'react-dom';
 import Excerpt from './';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Excerpt />, div);
-});
+[
+  [ '', 5, '...' ],
+  [ '', -2, '...' ],
+  [ 22, 0, '...' ],
+  [ {a: 1}, 0, '...' ],
+  [ 23.45, 0, '...' ],
+  [ , , '...' ],
+  [ null, null, '...' ],
+  [ 'one two three', 8, 'one two' ],
+].map(
+  ([content, maxChars, expected], index) => {
+    const container = document.createElement('div');
+    it(`renders the correct length substring ${index}`, () => {
+      render(
+        <Excerpt
+          content={content}
+          maxChars={maxChars}
+        />,
+        container
+      );
+      expect(container.textContent).toBe(expected);
+    });
+    // Cleanup.
+    unmountComponentAtNode(container);
+    container.remove();
+  }
+);
