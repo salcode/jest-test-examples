e3e2dc0081ad341152a3cc7516419b54b0d44aad
Sal Ferrarello <sal@ironcodestudio.com>
2021-02-26 11:04:07 -0500

    Add stub Excerpt component
    See #3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Credits from './components/Credits';
+import Excerpt from './components/Excerpt';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Excerpt />
       <Credits />
     </div>
   );
---
+++ src/components/Excerpt/index.js
@@ -0,0 +1,7 @@
+export default function Excerpt() {
+  return (
+    <div>
+      Excerpt component output
+    </div>
+  );
+}
