53c38c44fcacfe6744ca3d07f2af5cab082c4e14
Sal Ferrarello <sal@ironcodestudio.com>
2021-03-26 14:16:48 -0400

    Add fetch for CatFact

--- src/components/CatFact/index.js
+++ src/components/CatFact/index.js
@@ -1,5 +1,26 @@
+import {
+  useEffect,
+  useState
+} from 'react';
+
+const getCatFact = () => {
+  return fetch('https://cat-fact.herokuapp.com/facts/random')
+    .then(response => response.json())
+    .then(
+      (catFactObj) => catFactObj.text
+    );
+};
+
 export default function CatFact() {
+  const [catFact, setCatFact] = useState('Loading...');
+  useEffect(() => {
+    getCatFact()
+      .then(
+        (catFactText) => setCatFact(catFactText)
+      );
+  }, []);
+
   return (
-    <div>Cats are not dogs</div>
+    <div>{catFact}</div>
   );
 }
