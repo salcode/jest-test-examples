a70ec46badd7c4a909d7f05c3b1f08c240159f18
Sal Ferrarello <sal@ironcodestudio.com>
2021-03-26 12:35:41 -0400

    Add tests to modify input field and confirm output
    See #7

--- src/components/Echo/index.test.js
+++ src/components/Echo/index.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 import Echo from './';
 
 test(`Test Rendering Default`, () => {
@@ -9,3 +10,24 @@ test(`Test Rendering Default`, () => {
   // Check rendered element (h2) with default state value.
   expect(screen.getByText(defaultValueFromComponent)).toBeDefined();
 });
+
+test.each([
+  'Hello, World!',
+  'Hi everybody!',
+  ' racecar ',
+  '  ',
+  'one          two  ',
+  '<strong>weak</strong>',
+])(
+  'renders "%s" when put in input field',
+  (text) => {
+    render(<Echo />);
+    // Change text in textbox.
+    userEvent.clear(screen.getByRole('textbox'));
+    userEvent.type(screen.getByRole('textbox'), text);
+    // Verify text in textbox is updated.
+    expect(screen.getByRole('textbox')).toHaveValue(text);
+    // Verify rendered element (h2) has updated text.
+    expect(screen.getByRole('heading').textContent).toEqual(text);
+  }
+);
