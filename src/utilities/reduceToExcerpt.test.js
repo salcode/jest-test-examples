aca8d8a9c2c5394654c5ffa6937391f7170e89d5
Sal Ferrarello <sal@ironcodestudio.com>
2021-04-05 22:21:28 -0400

    Replace Array.map with Jest .each
    Instead of creating our own loop to test each of the lines in a set of
    data, we are now using the Jest .each() method.
    
    Resolves #14

--- src/utilities/reduceToExcerpt.test.js
+++ src/utilities/reduceToExcerpt.test.js
@@ -1,6 +1,6 @@
 import reduceToExcerpt from './reduceToExcerpt.js';
 
-[
+describe.each([
   [ 'Hi everybody', 4, 'Hi' ],
   [ '12 3456', 2, '12' ],
   [ 'One two three', 4, 'One' ],
@@ -15,9 +15,9 @@ import reduceToExcerpt from './reduceToExcerpt.js';
   [ 'One ', 4, 'One' ],
   [ ' One ', 5, 'One' ],
   [ 'encyclopedia', 5, '' ],
-].map(
-  ([content, maxChars, expected], index) => {
-    it(`renders the correct length substring ${index}`, () => {
+])('"%s" with maxChars %i',
+  (content, maxChars, expected) => {
+    test(`returns "${expected}"`, () => {
       expect(
         reduceToExcerpt(content, maxChars)
       ).toBe(expected);
