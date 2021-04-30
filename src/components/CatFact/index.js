38014951b094209398cd5efcff8f6cf17b55643b
Sal Ferrarello <sal@ironcodestudio.com>
2021-04-30 09:53:49 -0400

    Add getCatFact() function
    This function retrieves 10 random cat facts (which may or may not be
    verified), then we filter out the un-verified facts, then take the first
    of the remaining cat facts.
    
    See #13

--- src/components/CatFact/index.js
+++ src/components/CatFact/index.js
@@ -1,3 +1,14 @@
+const getCatFact = () =>
+  fetch('https://cat-fact.herokuapp.com/facts/random?amount=10')
+    .then(response => response.json())
+    .then(catFactObjects => catFactObjects.filter(
+      obj => obj.status.verified === true
+    ))
+    .then(verifiedCatFactObjects => verifiedCatFactObjects[0])
+    .then(
+      catFact => catFact?.text
+    );
+
 export default function CatFact() {
   return (
     <h4>Placeholder Cat Fact</h4>
